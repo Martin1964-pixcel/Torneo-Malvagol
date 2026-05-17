6f87af2370da476ddab40e740d82a8b2bedb85e5
Martin <marlbe2000@hotmail.com>
2026-05-17 16:55:15 -0600

    Proteccion admin

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const auth = request.headers.get("authorization");
+
+  const username = "admin";
+  const password = "malvagol2026";
+
+  if (auth) {
+    const encoded = auth.split(" ")[1];
+    const decoded = atob(encoded);
+
+    const [user, pass] = decoded.split(":");
+
+    if (user === username && pass === password) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Autenticación requerida", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Admin Area"',
+    },
+  });
+}
